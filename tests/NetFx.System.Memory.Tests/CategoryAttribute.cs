f9faedab0317809cdae35f7fe2a5010f628e35b9
Shad Storhaug <shad@shadstorhaug.com>
2024-03-26 13:24:29 +0700

    Added test project for NetFx.System.Memory, along with Base64 tests and a custom OuterLoop attribute

---
+++ tests/NetFx.System.Memory.Tests/CategoryAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace System
+{
+    [TraitDiscoverer("System.CategoryDiscoverer", "NetFx.System.Memory.Tests")]
+    [AttributeUsage(AttributeTargets.Method)]
+    public abstract class CategoryAttribute : Attribute, ITraitAttribute
+    {
+        public abstract string Type { get; }
+    }
+
+
+    public class CategoryDiscoverer : ITraitDiscoverer
+    {
+        private const string Key = "Category";
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            var attributeInfo = traitAttribute as ReflectionAttributeInfo;
+            var category = attributeInfo?.Attribute as CategoryAttribute;
+            var value = category?.Type ?? string.Empty;
+            yield return new KeyValuePair<string, string>(Key, value);
+        }
+    }
+
+}
---
+++ tests/NetFx.System.Memory.Tests/OuterLoopAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xunit.Sdk;
+
+namespace System
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, AllowMultiple = false)]
+    public class OuterLoopAttribute : CategoryAttribute
+    {
+        public override string Type => "OuterLoop";
+    }
+}
