9dc73c6f8f45b394a7f41cfb1978487429835bce
Shad Storhaug <shad@shadstorhaug.com>
2024-03-28 09:51:59 +0700

    Removed Assembly location line for files that include it

--- src/NetFx.System.Memory/System.Private.CoreLib/System/Buffers/Text/Utf8Constants.cs
+++ src/NetFx.System.Memory/System.Private.CoreLib/System/Buffers/Text/Utf8Constants.cs
@@ -2,7 +2,6 @@
 // Type: System.Buffers.Text.Utf8Constants
 // Assembly: System.Memory, Version=4.0.1.2, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51
 // MVID: 866AE087-4753-44D8-B4C3-B8D9EAD86168
-// Assembly location: F:\Users\shad\source\repos\CheckSystemMemoryDependencies\CheckSystemMemoryDependencies\bin\Debug\net45\System.Memory.dll
 
 namespace System.Buffers.Text
 {
--- src/NetFx.System.Memory/System/ThrowHelper.cs
+++ src/NetFx.System.Memory/System/ThrowHelper.cs
@@ -2,7 +2,6 @@
 // Type: System.ThrowHelper
 // Assembly: System.Memory, Version=4.0.1.2, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51
 // MVID: 866AE087-4753-44D8-B4C3-B8D9EAD86168
-// Assembly location: F:\Users\shad\source\repos\CheckSystemMemoryDependencies\CheckSystemMemoryDependencies\bin\Debug\net45\System.Memory.dll
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
