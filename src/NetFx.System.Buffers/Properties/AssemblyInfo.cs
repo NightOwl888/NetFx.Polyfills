f2d1d129c10daa505bb5a562502d2818636055fd
Shad Storhaug <shad@shadstorhaug.com>
2024-03-29 14:15:14 +0700

    Added AssemblyInfo for System.Buffers

---
+++ src/NetFx.System.Buffers/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+//[assembly: AssemblyCompany("Microsoft Corporation")]
+//[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyDefaultAlias("System.Buffers")]
+[assembly: AssemblyDescription("System.Buffers")]
+//[assembly: AssemblyFileVersion("4.6.28619.01")]
+//[assembly: AssemblyInformationalVersion("4.6.28619.01 @BuiltBy: dlab14-DDVSOWINAGE069 @Branch: release/2.1 @SrcCode: https://github.com/dotnet/corefx/tree/7601f4f6225089ffb291dc7d58293c7bbf5c5d4f")]
+//[assembly: AssemblyMetadata(".NETFrameworkAssembly", "")]
+//[assembly: AssemblyMetadata("PreferInbox", "True")]
+//[assembly: AssemblyMetadata("Serviceable", "True")]
+//[assembly: AssemblyProduct("Microsoft® .NET Framework")]
+//[assembly: AssemblyTitle("System.Buffers")]
+//[assembly: AssemblyVersion("4.0.3.0")]
+[assembly: CLSCompliant(true)]
+[assembly: CompilationRelaxations(8)]
+[assembly: Debuggable(DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
