39b0bcb623da86a8b68fd1e07cd41eaf596ba6f7
Shad Storhaug <shad@shadstorhaug.com>
2024-03-29 18:21:26 +0700

    NetFx.System.Memory: Auto-generate AssemblyDescriptionAttribute

--- src/NetFx.System.Memory/Properties/AssemblyInfo.cs
+++ src/NetFx.System.Memory/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyCompany("Microsoft Corporation")]
 //[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
-[assembly: AssemblyDefaultAlias("System.Memory")]
-[assembly: AssemblyDescription("System.Memory")]
+//[assembly: AssemblyDefaultAlias("System.Memory")] // NetFx: Removing alias to prevent any confusion, since we have a different strong name key than Microsoft
+//[assembly: AssemblyDescription("System.Memory")]
 //[assembly: AssemblyFileVersion("4.6.31308.01")]
 //[assembly: AssemblyInformationalVersion("4.6.31308.01 @BuiltBy: cloudtest-841353dfc000000 @Branch: release/2.1-MSRC @SrcCode: https://github.com/dotnet/corefx/tree/32b491939fbd125f304031c35038b1e14b4e3958")]
 //[assembly: AssemblyMetadata(".NETFrameworkAssembly", "")]
