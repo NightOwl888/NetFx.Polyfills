99b5970e55b67fcc65210cc6094b608e4cec2788
Shad Storhaug <shad@shadstorhaug.com>
2024-03-29 18:21:05 +0700

    NetFx.System.Buffers: Auto-generate AssemblyDescriptionAttribute

--- src/NetFx.System.Buffers/Properties/AssemblyInfo.cs
+++ src/NetFx.System.Buffers/Properties/AssemblyInfo.cs
@@ -6,8 +6,8 @@ using System.Runtime.CompilerServices;
 
 //[assembly: AssemblyCompany("Microsoft Corporation")]
 //[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
-[assembly: AssemblyDefaultAlias("System.Buffers")]
-[assembly: AssemblyDescription("System.Buffers")]
+//[assembly: AssemblyDefaultAlias("System.Buffers")] // NetFx: Removing alias to prevent any confusion, since we have a different strong name key than Microsoft
+//[assembly: AssemblyDescription("System.Buffers")]
 //[assembly: AssemblyFileVersion("4.6.28619.01")]
 //[assembly: AssemblyInformationalVersion("4.6.28619.01 @BuiltBy: dlab14-DDVSOWINAGE069 @Branch: release/2.1 @SrcCode: https://github.com/dotnet/corefx/tree/7601f4f6225089ffb291dc7d58293c7bbf5c5d4f")]
 //[assembly: AssemblyMetadata(".NETFrameworkAssembly", "")]
