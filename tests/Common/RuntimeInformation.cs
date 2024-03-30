f9304e1f53d0cc82eb04b4dce75c2e4dd7d63c1d
Shad Storhaug <shad@shadstorhaug.com>
2024-03-30 10:14:21 +0700

    Reworked build to fix build errors on net462. For now, we are not testing on net452.

---
+++ tests/Common/RuntimeInformation.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+#if !FEATURE_RUNTIMEINFORMATION
+namespace System.Runtime.InteropServices
+{
+    public static class RuntimeInformation
+    {
+        private const string FrameworkName = ".NET Framework";
+
+        private static string s_frameworkDescription;
+
+        public static string FrameworkDescription
+        {
+            get
+            {
+                if (s_frameworkDescription == null)
+                {
+                    AssemblyFileVersionAttribute assemblyFileVersionAttribute = (AssemblyFileVersionAttribute)typeof(object).GetTypeInfo().Assembly.GetCustomAttribute(typeof(AssemblyFileVersionAttribute));
+                    s_frameworkDescription = FrameworkName + assemblyFileVersionAttribute.Version;
+                }
+
+                return s_frameworkDescription;
+            }
+        }
+    }
+}
+#endif
